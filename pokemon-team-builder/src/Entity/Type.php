f50461ed9799dda204a40fef636efb89d6959cbc
Emilie VITAL <emilievital@yahoo.fr>
2021-02-10 10:34:19 +0100

    Modification of column names on tables neutral to, immune to, vulnerable to, resistant to

--- pokemon-team-builder/src/Entity/Type.php
+++ pokemon-team-builder/src/Entity/Type.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\JoinTable;
+use Doctrine\ORM\Mapping\JoinColumn;
 
 /**
  * @ORM\Entity(repositoryClass=TypeRepository::class)
@@ -30,14 +31,13 @@ class Type
      */
     private $image;
 
-    /**
-     * @ORM\ManyToMany(targetEntity=Pokemon::class, inversedBy="types")
-     */
-    private $pokemon;
 
     /**
      * @ORM\ManyToMany(targetEntity=Type::class, inversedBy="vulnerability")
-     * @JoinTable(name="vulnerable_to")
+     * @JoinTable(name="vulnerable_to",
+     *      joinColumns={@JoinColumn(name="attacked_type", referencedColumnName="id")},
+     *      inverseJoinColumns={@JoinColumn(name="attacking_type", referencedColumnName="id")}
+     *      )
      */
     private $vulnerable_to;
 
@@ -48,7 +48,10 @@ class Type
 
     /**
      * @ORM\ManyToMany(targetEntity=Type::class, inversedBy="resistance")
-     * @JoinTable(name="resistant_to")
+     * @JoinTable(name="resistant_to",
+     *      joinColumns={@JoinColumn(name="attacked_type", referencedColumnName="id")},
+     *      inverseJoinColumns={@JoinColumn(name="attacking_type", referencedColumnName="id")}
+     *      )
      */
     private $resistant_to;
 
@@ -59,7 +62,10 @@ class Type
 
     /**
      * @ORM\ManyToMany(targetEntity=Type::class, inversedBy="neutrality")
-     * @JoinTable(name="neutral_to")
+     * @JoinTable(name="neutral_to",
+     *      joinColumns={@JoinColumn(name="attacked_type", referencedColumnName="id")},
+     *      inverseJoinColumns={@JoinColumn(name="attacking_type", referencedColumnName="id")}
+     *      )
      */
     private $neutral_to;
 
@@ -70,7 +76,10 @@ class Type
 
     /**
      * @ORM\ManyToMany(targetEntity=Type::class, inversedBy="immunity")
-     * @JoinTable(name="immune_to")
+     * @JoinTable(name="immune_to",
+     *      joinColumns={@JoinColumn(name="attacked_type", referencedColumnName="id")},
+     *      inverseJoinColumns={@JoinColumn(name="attacking_type", referencedColumnName="id")}
+     *      )
      */
     private $immune_to;
 
@@ -123,30 +132,7 @@ class Type
         return $this;
     }
 
-    /**
-     * @return Collection|Pokemon[]
-     */
-    public function getPokemon(): Collection
-    {
-        return $this->pokemon;
-    }
-
-    public function addPokemon(Pokemon $pokemon): self
-    {
-        if (!$this->pokemon->contains($pokemon)) {
-            $this->pokemon[] = $pokemon;
-        }
-
-        return $this;
-    }
-
-    public function removePokemon(Pokemon $pokemon): self
-    {
-        $this->pokemon->removeElement($pokemon);
-
-        return $this;
-    }
-
+    
     /**
      * @return Collection|self[]
      */
