04067a6a305d1ea6f9728352a21b2b9b096bdfe9
Emilie VITAL <emilievital@yahoo.fr>
2021-02-12 14:29:41 +0100

    api route to find a pokemon by id

--- pokemon-team-builder/src/Controller/Api/V1/ApiController.php
+++ pokemon-team-builder/src/Controller/Api/V1/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Api\V1;
 
+use App\Entity\Pokemon;
 use App\Repository\PokemonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,5 +25,14 @@ class ApiController extends AbstractController
         return $this->json($pokemons);
     }
 
-    
+    /**
+     * @Route("/{id}", name="show_by_id", methods={"GET"})
+     */
+    public function read(Pokemon $pokemon): Response
+    {
+        return $this->json($pokemon);
+    }
+
+
+
 }
