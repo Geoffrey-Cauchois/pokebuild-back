cf03d2849e597d7c1e72994c203904c437a5cb05
Emilie VITAL <emilievital@yahoo.fr>
2021-02-16 13:03:54 +0100

    modification of class PokemonServiceTest, using KernelTestCase in order to access the service container

--- pokemon-team-builder/tests/Controller/ApiControllerTest.php
+++ pokemon-team-builder/tests/Controller/ApiControllerTest.php
@@ -15,4 +15,26 @@ class ApiControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         
     }
+
+    public function testPokemonId(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET',
+         'http://localhost/api/v1/pokemon/840');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertStringContainsString('"id":840', $client->getResponse()->getContent()); 
+        
+    }
+
+    public function testPokemonName(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET',
+         'http://localhost/api/v1/pokemon/Grillepattes');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertStringContainsString('Grillepattes', $client->getResponse()->getContent());
+        
+    }
 }
--- pokemon-team-builder/tests/Service/PokemonServiceTest.php
+++ pokemon-team-builder/tests/Service/PokemonServiceTest.php
@@ -3,19 +3,30 @@
 namespace App\Tests\Service;
 
 use App\Entity\Pokemon;
-use App\Repository\TypeRepository;
-use PHPUnit\Framework\TestCase;
+use App\Repository\PokemonRepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use App\Service\PokemonService;
 
-class PokemonServiceTest extends TestCase
+class PokemonServiceTest extends KernelTestCase
 {
-    public function testPokemonService(): void
+    private $pokemonService;
+    private $pokemonRepository;
+
+    protected function setUp(): void
     {
-        $pokemonToTest = new Pokemon;
-        $pokemonToTest->getId('721');
-        $pokemonService = new PokemonService;
+        self::bootKernel();
+        $this->pokemonService = self::$container->get(PokemonService::class);
+        $this->pokemonRepository = self::$container->get(PokemonRepository::class);
+
+
+    }
+
+    public function testPokemonService() 
+    {
+        $pokemonToTest = $this->pokemonRepository->find(850);
+        $this->pokemonService->calculateResistances($pokemonToTest);
+        $this->assertEquals($pokemonToTest->getResistances()['Roche']['damage_multiplier'], 4);
+
         
-        $typeToTest = $pokemonToTest>getType('Roche');
-        $this->assertTrue(true);
     }
 }
