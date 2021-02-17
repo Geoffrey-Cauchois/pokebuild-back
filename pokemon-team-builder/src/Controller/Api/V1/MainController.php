919b86da3cf2d8e1363ef4c40e9eab81fae60003
Emilie VITAL <emilievital@yahoo.fr>
2021-02-17 15:44:50 +0100

    homepage + links to the different api routes

--- pokemon-team-builder/src/Controller/Api/V1/MainController.php
+++ pokemon-team-builder/src/Controller/Api/V1/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Api\V1;
 
+use App\Repository\PokemonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/api/v1/main", name="api_v1_main")
+     * @Route("/api/v1", name="api_v1_")
      */
     public function index(): Response
     {
+        
         return $this->render('api/v1/main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
