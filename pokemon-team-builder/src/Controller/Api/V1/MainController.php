d2dc8b5ab0f447ff1619ab312491d5fe60177269
Geoffrey Cauchois <geoffrey.cauchois@gmail.com>
2021-02-18 17:16:27 +0100

    first-step-test-defensieCover

--- pokemon-team-builder/src/Controller/Api/V1/MainController.php
+++ pokemon-team-builder/src/Controller/Api/V1/MainController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller\Api\V1;
 
+use App\Form\TeamType;
 use App\Repository\PokemonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MainController extends AbstractController
 {
@@ -19,5 +23,56 @@ class MainController extends AbstractController
         return $this->render('api/v1/main/index.html.twig', [
             'pokemons' => $allPokemon,
         ]);
+    }
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): RedirectResponse
+    {
+      return $this->redirectToRoute('api_v1_');
+    }
+
+    /**
+     * @Route("/defensiveCoverTest", name="defensie-cover-test")
+     */
+    public function defensieCoverageTest(Request $request, HttpClientInterface $client)
+    {
+      $form = $this->createForm(TeamType::class);
+
+      $form->handleRequest($request);
+
+      if($form->isSubmitted() && $form->isValid()){
+
+        $team = $form->getData();
+
+        $chosenPokemon = $team->getPokemon();
+
+        foreach($chosenPokemon as $pokemon){
+
+          $chosenPokemonIds[] = $pokemon->getId();
+        }
+
+        $chosenPokemonIds = json_encode($chosenPokemonIds);
+
+        dump($chosenPokemonIds);
+
+        $opts = ['http' => [
+                            'method' => 'POST',
+                            'header' => 'Content-type: application/x-www-form-urlencoded',
+                            'content' => $chosenPokemonIds
+        ]];
+
+        $context = stream_context_create($opts);
+
+        $processedCoverage = file_get_contents('http://ec2-3-83-51-192.compute-1.amazonaws.com/api/v1/team/defensive-coverage', false, $context);
+
+        $decodedProcessedCoverage = json_decode($processedCoverage);
+
+        return $this->json($decodedProcessedCoverage);
+      }
+
+      return $this->render('api/v1/main/defensiveCoverage.html.twig', [
+        'form' => $form->createView()
+    ]);
     }
 }
