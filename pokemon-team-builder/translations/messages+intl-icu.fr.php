a16506d07e9468b100f01888e51da6db204fc87f
Geoffrey Cauchois <geoffrey.cauchois@gmail.com>
2021-02-23 14:23:53 +0100

    conflict-resolve

--- pokemon-team-builder/translations/messages+intl-icu.fr.php
+++ pokemon-team-builder/translations/messages+intl-icu.fr.php
@@ -14,6 +14,8 @@ return [
   'user-creation' => 'Utilisateur {user} créé',
   'user-deletion' => 'Utilisateur {user} supprimé',
   'user-edition' => 'Utilisateur {user} modifié',
+  'existing-user' => 'Ce nom d\'utilisateur existe déjà',
+  'wrong-email' => 'Adresse e-mail incorrecte',
   'wrong-team-name' => 'Equipe inconnue.',
   'team-creation' => 'Equipe {team} créée',
   'team-deletion' => 'Equipe {team} supprimée',
