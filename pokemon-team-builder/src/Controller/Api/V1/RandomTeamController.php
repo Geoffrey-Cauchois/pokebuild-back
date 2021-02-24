ff3ed7b6a4c4d1b56693fa183e5d37c3d0c7304a
Emilie VITAL <emilievital@yahoo.fr>
2021-02-24 10:17:06 +0100

    adding random team

---
+++ pokemon-team-builder/src/Controller/Api/V1/RandomTeamController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Api\V1;
+
+use App\Repository\PokemonRepository;
+use App\Service\PokemonService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RandomTeamController extends AbstractController
+{
+    /**
+     * @Route("/api/v1/random/team", name="api_v1_random_team", methods={"GET"})
+     */
+    public function random(PokemonRepository $pokemonRepository, PokemonService $pokemonService): Response
+    {
+
+        $randomTeam = [];
+        for ($i = 0; $i < 7; $i ++) {
+            $randomPokemon = $pokemonRepository->find(rand(1,898));
+                if (!in_array($randomPokemon, $randomTeam)) {
+                    
+                    $randomTeam[] = $randomPokemon;
+                }         
+        }
+
+        foreach ($randomTeam as $pokemon){
+            $pokemonService->calculateResistances($pokemon);
+        }
+
+        return $this->json($randomTeam);
+     
+    }
+}
