0a1b273b643f826274fde5dd2986cf63074f8181
Emilie VITAL <emilievital@yahoo.fr>
2021-02-25 10:55:43 +0100

    resolve conflict

--- pokemon-team-builder/translations/messages+intl-icu.fr.php
+++ pokemon-team-builder/translations/messages+intl-icu.fr.php
@@ -23,5 +23,6 @@ return [
   'team-edition' => 'Equipe {team} modifiée',
   'non-matching-passwords' => 'Les deux mots de passe ne correspondent pas.',
   'login' => 'Connexion réussie. Bienvenue {user}.',
-  'wrong-pokemon' => 'Pokemon inconnu.'
+  'wrong-pokemon' => 'Pokemon inconnu.',
+  'user-edition-null' => 'Requête réussie pour l\'utilisateur {user}, aucune donnée à modifier'
 ];
