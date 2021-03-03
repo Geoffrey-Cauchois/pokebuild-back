a294cedaa295705e2b8062a382f299a6c4517f68
Geoffrey Cauchois <geoffrey.cauchois@gmail.com>
2021-03-03 14:51:57 +0100

    more-tests

--- pokemon-team-builder/tests/Controller/MainControllerTest.php
+++ pokemon-team-builder/tests/Controller/MainControllerTest.php
@@ -16,7 +16,13 @@ class MainControllerTest extends WebTestCase
         
     }
 
-        
+    public function testHome()
+    {
+      $client = static::createClient();
+      $crawler = $client->request('GET', '/api/v1');
+
+      $this->assertSelectorTextContains('div.card:nth-child(10) > div:nth-child(2) > p:nth-child(2) > strong:nth-child(1)', 'double-weakness');
+    } 
 }
 
 
--- pokemon-team-builder/tests/Service/PokemonServiceTest.php
+++ pokemon-team-builder/tests/Service/PokemonServiceTest.php
@@ -37,6 +37,32 @@ class PokemonServiceTest extends KernelTestCase
   
     }
 
-   
+    public function testCoverage()
+    {
+
+      $pokemonToTestIds = [455, 149, 12];
+
+      $testedTeam = $this->pokemonService->calculateDefensiveCoverage($pokemonToTestIds);
+
+      $this->assertEquals($testedTeam->getDefensiveCover()['Vol']['result'], 'vulnerable');
+      $this->assertEquals($testedTeam->getDefensiveCover()['Feu']['result'], 'slightly-vulnerable');
+      $this->assertEquals($testedTeam->getDefensiveCover()['Acier']['result'], 'balanced');
+      $this->assertEquals($testedTeam->getDefensiveCover()['Plante']['result'], 'resistant');
+    }
+
+    public function testSuggestion()
+    {
+
+      $pokemonToTestIds = [15, 272, 400];
+
+      $testedSuggestions = $this->pokemonService->suggestPokemon($pokemonToTestIds);
+
+      foreach($testedSuggestions as $suggestedPokemon){
+        $this->assertContains($suggestedPokemon->getResistances()['Vol']['damage_multiplier'], [0, 0.25, 0.5]);
+        $this->assertContains($suggestedPokemon->getResistances()['Psy']['damage_multiplier'], [0, 0.25, 0.5]);
+        $this->assertNotContains($suggestedPokemon->getResistances()['Glace']['damage_multiplier'], [4]);
+        $this->assertNotContains($suggestedPokemon->getResistances()['Poison']['damage_multiplier'], [2, 4]);
+      }
+    }
 
 }
